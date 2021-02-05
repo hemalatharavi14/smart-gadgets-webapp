950c00fe47a2958cef0107e94cd80db1ab5b2ed3
Hemalatha Ravi <hemalatharavi14@gmail.com>
2021-02-05 12:15:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
 include_once("index.html");
-include_once("style.css");
-include_once("script.js");
-include_once("arrow.jpg");
-include_once("chat_icon.jpg");
-include_once("gadgets.jpg");
-include_once("logo.gif");
-include_once("web_icon.jpg");
 ?>
